4176d23ad4f1517b57d6398c69a85caa3d7bf12e
mrkhaled257@gmail.com <khaledadeeb>
2023-05-23 20:25:34 +0300

    first commit

---
+++ ثيمات/ثيمات/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ثيمات.Models;
+
+namespace ثيمات.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult about()
+        {
+            return View();
+        }
+        public IActionResult blog()
+        {
+            return View();
+        }
+        public IActionResult contact()
+        {
+            return View();
+        }
+
+       
+        public IActionResult services()
+        {
+            return View();
+        }
+        public IActionResult single()
+        {
+            return View();
+        }
+
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
